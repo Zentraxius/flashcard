2a6558afdc61c5dac2b9ad9f18b2b405130570bf
Cody Fritz <clanalia55@gmail.com>
2020-06-02 13:54:38 -0700

    add alert information to buttons and changed button color
    co-authored-by: Jerrod Styrk <jstyrk@citadel.edu>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
   $("button#JavaScript").click(function () {
     $("body").removeClass();
-    alert("");
+    alert(
+      "An object-oriented computer programming language commonly used to create interactive effects within web browsers."
+    );
   });
 
   $("button#Operators").click(function () {
@@ -25,7 +27,9 @@ $(document).ready(function () {
 
   $("button#Functions").click(function () {
     $("body").removeClass();
-    alert("");
+    alert(
+      "A function is a block of code that performs an action and returns a result; optionally takes arguments"
+    );
   });
 
   $("button#Methods").click(function () {
@@ -44,12 +48,14 @@ $(document).ready(function () {
 
   $("button#Parameters").click(function () {
     $("body").removeClass();
-    alert("");
+    alert("Is a variable that's assigned to the argument.");
   });
 
   $("button#Return").click(function () {
     $("body").removeClass();
-    alert("");
+    alert(
+      "Once called, many functions will provide a result of some kind. This is known as returning, and the result is often referred to as a return value."
+    );
   });
 
   $("button#Chaining methods").click(function () {
@@ -76,7 +82,7 @@ $(document).ready(function () {
 
   $("button#NaN").click(function () {
     $("body").removeClass();
-    alert("");
+    alert("Not a Number");
   });
 
   $("button#Null").click(function () {
@@ -86,21 +92,58 @@ $(document).ready(function () {
 
   $("button#Alert").click(function () {
     $("body").removeClass();
-    alert("");
+    alert("Displays a window. Opens a dialog box and returns undefined.");
   });
 
   $("button#Comments").click(function () {
     $("body").removeClass();
-    alert("");
+    alert(
+      "Anything following // on a line in JavaScript is ignored by the computer. Comments are a convenient way to leave notes in your code for yourself or other programmers."
+    );
   });
 
   $("button#jQuery").click(function () {
     $("body").removeClass();
-    alert("");
+    alert("Is a Javascript library");
   });
 
   $("button#Attributes").click(function () {
     $("body").removeClass();
-    alert("");
+    alert(
+      "Specifies one or more classnames for an element. The class attribute is mostly used to point to a class in a style sheet. However, it can also be used by a JavaScript (via the HTML DOM) to make changes to HTML elements with a specified class."
+    );
   });
 });
+
+{
+  /* <button class="btn btn-danger" id="red">
+  Red
+</button>;
+$(document).ready(function () {
+  $("button#green").click(function () {
+    $("body").removeClass("yellow-background", "red-background");
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function () {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function () {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+}); */
+}
+
+// <div id="walrus-hidden">
+// <p>Let's play peek-a-boo. <span class="clickable">Click here</span> to see the surprise!</p>
+// </div>
+
+// $(document).ready(function () {
+//  $(".clickable").click(function () {
+//    $("#walrus-showing").slideToggle();
+//   $("#walrus-hidden").slideToggle();
+//  });
+// });
