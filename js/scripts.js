e1383e7e8a9ccc896985553040235981b782b115
Cody Fritz <clanalia55@gmail.com>
2020-06-02 13:22:25 -0700

    add initial commit, index.html setup and scripts.js mostly setup
    Co-Authored-By Jerrod Styrk <j.styrk@citadel.edu>

---
+++ js/scripts.js
@@ -0,0 +1,106 @@
+$(document).ready(function () {
+  $("button#JavaScript").click(function () {
+    $("body").removeClass();
+    alert("");
+  });
+
+  $("button#Operators").click(function () {
+    $("body").removeClass();
+    alert(
+      "+, -, *, /, and % are called operators. An operator is a special character (or characters) that indicates an action to be performed."
+    );
+  });
+
+  $("button#Variables").click(function () {
+    $("body").removeClass();
+    alert(
+      "Variables can be thought of as containers used to store information. They allow for a way to label data with a descriptive name."
+    );
+  });
+
+  $("button#Variable naming conventions").click(function () {
+    $("body").removeClass();
+    alert("");
+  });
+
+  $("button#Functions").click(function () {
+    $("body").removeClass();
+    alert("");
+  });
+
+  $("button#Methods").click(function () {
+    $("body").removeClass();
+    alert(
+      "A method is an action run on a piece of data; you can think of it as a message you send to a piece of data, and the result is the response."
+    );
+  });
+
+  $("button#Arguments").click(function () {
+    $("body").removeClass();
+    alert(
+      "Some methods take one or more arguments that provide the method with additional information to help it perform its action."
+    );
+  });
+
+  $("button#Parameters").click(function () {
+    $("body").removeClass();
+    alert("");
+  });
+
+  $("button#Return").click(function () {
+    $("body").removeClass();
+    alert("");
+  });
+
+  $("button#Chaining methods").click(function () {
+    $("body").removeClass();
+    alert("Calling a method directly on the return value of another method.");
+  });
+
+  $("button#Strings").click(function () {
+    $("body").removeClass();
+    alert(
+      'A string is a set of characters enclosed in quotations. "hello" is an example'
+    );
+  });
+
+  $("button#Booleans").click(function () {
+    $("body").removeClass();
+    alert("A boolean is either true or false.");
+  });
+
+  $("button#Undefined").click(function () {
+    $("body").removeClass();
+    alert("This means something hasn't been defined in our code yet.");
+  });
+
+  $("button#NaN").click(function () {
+    $("body").removeClass();
+    alert("");
+  });
+
+  $("button#Null").click(function () {
+    $("body").removeClass();
+    alert("Null is nothing.");
+  });
+
+  $("button#Alert").click(function () {
+    $("body").removeClass();
+    alert("");
+  });
+
+  $("button#Comments").click(function () {
+    $("body").removeClass();
+    alert("");
+  });
+
+  $("button#jQuery").click(function () {
+    $("body").removeClass();
+    alert("");
+  });
+
+  $("button#Attributes").click(function () {
+    $("body").removeClass();
+    alert("");
+  });
+});
